bc7cd0183b038bbbb84052096a9881acbb3aa66e
heydenjq <jonas.heyden@informatik.hu-berlin.de>
2018-01-07 16:23:36 +0100

    corrected stupid permutation table error

--- spn.lua
+++ spn.lua
@@ -4,8 +4,8 @@ require "analyse"
 
 local transvl = 
     {1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12 , 16}
-local sboxaufg = sboxHex("8421C63DA5E7FB90")
-local spnvl = spn(16,kgenShift,sboxaufg,4,transvl,3)
+local sboxaufg = sboxHex("E213D906F45A8C7B")
+local spnvl = spn(16,kgenShift,sboxaufg,4,transvl,4)
 local kspnvl = hexStringToBitArray("4AA4")
 local xspnvl = hexStringToBitArray("26B7")
 
